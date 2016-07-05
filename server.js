1e182d5270c3c9183c4c0b41cc6a9ff4b4c8cab7
WindArrow <wind.dong.dream@gmail.com>
2016-07-05 17:26:17 +0900

    add condition

--- server.js
+++ server.js
@@ -1243,8 +1243,8 @@ io.on('connection', function(socket) {
 				'code' : 510
 			});
 		} else {
-			mySqlConnection.query('SELECT * FROM (timelines INNER JOIN users ON timelines.timeline_user_id = "' + userId + '" AND timelines.timeline_school_id = ' + schoolId
-					+ ') LEFT JOIN likes ON timelines.timeline_id = likes.like_timeline_id AND likes.like_user_id = "' + userId
+			mySqlConnection.query('SELECT * FROM timelines INNER JOIN users ON timelines.timeline_user_id = "' + userId + '" AND timelines.timeline_school_id = ' + schoolId + ' AND timelines.timeline_user_id = users.user_id'
+					+ ' LEFT JOIN likes ON timelines.timeline_id = likes.like_timeline_id AND likes.like_user_id = "' + userId
 					+ '" LEFT JOIN files ON files.file_parent_id = timelines.timeline_id ORDER BY timelines.timeline_id', function(err, timelineResult) {
 				if (err) {
 					console.error('내가 쓴 타임라인 요청 에러 = ' + err);
