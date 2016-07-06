5cd6bd82983ce6e52cbba54a82fd109e9702a072
WindArrow <wind.dong.dream@gmail.com>
2016-07-06 15:47:39 +0900

    getMySchoolRank

--- server.js
+++ server.js
@@ -852,7 +852,8 @@ io.on('connection', function(socket) {
 				console.info('내 학교 랭킹 요청 성공 = ' + JSON.stringify(result));
 				socket.emit('getMySchoolRanking', {
 					'code' : 200,
-					'school' : result[0]
+					result[0],
+					'hi' : 'hi'
 				});
 			}
 		});
